4d060c799580d7325c9fc5f67a3ca72ee4b14cba
estefanialucena <estefanialucena@hotmail.com>
2023-06-20 12:27:44 +0200

    estilos a todo

--- resources/views/cancion/formUpdate.blade.php
+++ resources/views/cancion/formUpdate.blade.php
@@ -1,3 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <link rel="Shortcut Icon" type="image/x-icon" href="../assets/icons/Page-icon-min.ico" />
+    <link href='http://fonts.googleapis.com/css?family=Denk+One' rel='stylesheet' type='text/css'>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="{{ asset('js/modernizr.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
+
+</head>
+
+<body class="container-body-pages container-index">
+    <div class="button-up hidden-xs">
+        <i class="fa fa-arrow-circle-up"></i> &nbsp; Ir arriba
+    </div>
+    <div class="principal-container-page">
+        <!-- ========= Menú ====== -->
+        <nav class="navigation-menu-container">
+            <div class="header-navigation">
+                Menú
+                <i class="fa fa-times btn-mobile-menu btn-navigation pull-right"></i>
+            </div>
+            <ul class="list-unstyled text-center">
+                @auth
+                        <li><a href="{{ url('/home') }}" class="all-titles-pages">Perfil</a></li>
+                    @else
+                        <li><a href="{{ route('login') }}" class="all-titles-pages">Log in</a></li>
+
+                        @if (Route::has('register'))
+                            <li><a href="{{ route('register') }}" class="all-titles-pages">Register</a></li>
+                        @endif
+                    @endauth
+            </ul>
+        </nav>
 <div class="box box-info padding-1">
     <div class="box-body">
         
@@ -27,4 +72,5 @@
     <div class="box-footer mt20">
         <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
     </div>
+</div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <link rel="Shortcut Icon" type="image/x-icon" href="../assets/icons/Page-icon-min.ico" />
+    <link href='http://fonts.googleapis.com/css?family=Denk+One' rel='stylesheet' type='text/css'>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="{{ asset('js/modernizr.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
+
+</head>
+
+<body class="container-body-pages container-index">
+    <div class="button-up hidden-xs">
+        <i class="fa fa-arrow-circle-up"></i> &nbsp; Ir arriba
+    </div>
+    <div class="principal-container-page">
+        <!-- ========= Menú ====== -->
+        <nav class="navigation-menu-container">
+            <div class="header-navigation">
+                Menú
+                <i class="fa fa-times btn-mobile-menu btn-navigation pull-right"></i>
+            </div>
+            <ul class="list-unstyled text-center">
+                @auth
+                        <li><a href="{{ url('/home') }}" class="all-titles-pages">Perfil</a></li>
+                    @else
+                        <li><a href="{{ route('login') }}" class="all-titles-pages">Log in</a></li>
+
+                        @if (Route::has('register'))
+                            <li><a href="{{ route('register') }}" class="all-titles-pages">Register</a></li>
+                        @endif
+                    @endauth
+            </ul>
+        </nav>
 <div class="container">
     <div class="table-responsive">
         <table class="table table-striped table-hover">
@@ -39,5 +84,6 @@
         </table>
     </div>
     {!! $canciones->links() !!}
+</div>
 </div>
 @endsection
